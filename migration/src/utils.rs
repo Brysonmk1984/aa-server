f9b6ea4440ce97dcaa3f2c8ba05ddd9b5fe36ff1
Bryson Kruk <brysonmk1984@gmail.com>
2023-11-14 07:34:09 -0700

    removed unused code

--- migration/src/utils.rs
+++ migration/src/utils.rs
@@ -23,32 +23,12 @@ pub enum Armies {
     ShieldRating,
     Flying,
     Range,
-    AttackSpeed,
-    Spread,
-    Accuracy,
-    Aoe,
-    WeaponType,
-    ArmorType,
-    Agility,
-    Speed,
+    // AttackSpeed,
+    // Spread,
+    // Accuracy,
+    // Aoe,
+    // WeaponType,
+    // ArmorType,
+    // Agility,
+    // Speed,
 }
-// pub id: i32,
-// pub name: String,
-// pub lore: String,
-// pub size: i32,
-// #[sea_orm(column_type = "Decimal(Some((3, 2)))")]
-// pub shield_rating: Decimal,
-// pub flying: bool,
-// pub range: i32,
-
-// #[sea_orm(column_type = "Decimal(Some((3, 2)))")]
-// pub attack_speed: Decimal,
-// #[sea_orm(column_type = "Decimal(Some((3, 2)))")]
-// pub accuracy: Decimal,
-// pub aoe: bool,
-
-// pub weapon_type: String,
-// pub armor_type: String,
-// #[sea_orm(column_type = "Decimal(Some((3, 2)))")]
-// pub agility: Decimal,
-// pub speed: i32,
